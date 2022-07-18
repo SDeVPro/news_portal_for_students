13f03e93d138d940ed24c2770b4c63cf1c9834b8
Sardor Abdulxamidov <web_software_developer@mail.ru>
2022-07-18 18:46:03 +0500

    manager

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from __future__ import unicode_literals 
 
 # Create your models here.
+class Main(models.Model):
+    name = models.CharField()
+    about = models.TextField()
+    abouttxt = models.TextField(default="")
+    fb = models.CharField(default="",max_length=50)
+    tw = models.CharField(default="",max_length=50)
+    yt = models.CharField(default="",max_length=50)
+    ins = models.CharField(default="",max_length=50)
+    tik = models.CharField(default="",max_length=50)
+    link = models.CharField(default="",max_length=50)
+    set_name = models.CharField(default="",max_length=50)
+
+    #for header and footer logo #tepa va pastki qism uchun logo
+    picurl = models.TextField(default="")
+    picname = models.TextField(default="")
+
+    picurl2 = models.TextField(default="")
+    picname2 = models.TextField(default="")
+
+    def __str__(self):
+        return self.set_name + "|"+str(self.pk)
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from __future__ import unicode_literals 
 
-# Create your models here.
+class Manager(models.Model):
+    name = models.CharField(max_length=50)
+    utxt = models.TextField()#username uchun text 
+    email = models.TextField(default="")
+    ip = models.TextField(default="")#get user ip address foydalanuvchi IP manzilini olish
+    country = models.TextField(default="")#get user location #foydalanuvchi turgan joyini aniqlash
+    def __str__(self):
+        return self.name 
+        
