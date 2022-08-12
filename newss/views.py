95a49ece053cc19b12f91121ea052a0a89398b09
Sardor Abdulxamidov <web_software_developer@mail.ru>
2022-08-12 18:55:35 +0500

    newss views

--- newss/views.py
+++ newss/views.py
@@ -146,4 +146,112 @@ def news_add(request):
             error = "Please Input Your Image"
             return render(request,'back/error.html',{'error':error})
     return render(request,'back/news_add.html',{'cat':cat})
+
+def news_delete(request,pk):
+    if not request.user.is_authenticated:
+        return redirect('mylogin')
     
+    perm = 0 
+    for i in request.user.groups.all():
+        if i.name == "masteruser":perm = 1 
+    
+    if perm == 0:
+        a = News.objects.get(pk=pk).writer
+        if str(a) != str(request.user):
+            error = "Access Denied"
+            return render(request,'back/error.html',{'error':error})
+    try:
+        b = News.objects.get(pk=pk)
+        fs = FileSystemStorage()
+        fs.delete(b.picname)
+        ocatid = News.objects.get(pk=pk).ocatid#To delete count of news after delete, ya'ni bu yangilik o'chgandan keyin, yangiliklar sonida o'zgarish bo'lishi kerak
+        b.delete()
+        count = len(News.objects.filter(ocatid=ocatid))#To delete  count of news after delete
+        m = Cat.objects.get(pk=ocatid)
+        m.count = count 
+        m.save()
+    except:
+        error = "Something Wrong"
+        return render(request,'back/error.html',{'error':error})
+    return redirect('news_list')
+
+def news_edit(request,pk):#yangilikni o'zgartirish
+    if not request.user.is_authenticated:#tekshiramiz, agar foydalanuvchi authenticatsiyadan o'tmagan bo'lsa if not (o'tmagan yo'q bo'lsa), demak bu foyalanuvchini 
+        return redirect('mylogin')#myloginga qaytarsin
+    if len(News.objects.filter(pk=pk))==0:# when the news does not exist ya'ni tanlangan yangilik bazadan topilmaganda 0  qiymat qaytarsa
+        error = "News Not Found"
+        return render(request,'back/error.html',{'error':error})
+    perm = 0 #huquq hozircha yo'q, admin huquqi
+    for i in request.user.groups.all():#barcha foydalanuvchi guruhlari ichida yurgunda
+        if i.name == "masteruser":perm = 1 #agar ismi masteruser bo'lsa va uning huquqi admin bo'lsa 
+    
+    if perm == 0:#agar huquq 0 bo'lib qolsa, demak 
+        a = News.objects.get(pk=pk).writer#demak bu Yangilik muallifi
+        if str(a) != str(request.user):#agar yangilik muallifi teng bo'lmasa kirgan foydalanuvchiga, ya'ni anvar =teng emas yanvarga 
+            error = "Access Denied"#kirish ta'qiqlangan, mumkin emas
+            return render(request,'back/error.html',{'error':error})
+
+    news = News.objects.get(pk=pk)#aynan tanlangan yangilikni olib kelamiz
+    cat = SubCat.objects.all()#categorysi olib kelinsin hammasi
+    if request.method == "POST":#Editga kirishimiz kerak, kirishga va o'zgartirishga ruxsat berildi
+        newstitle = request.POST.get('newstitle')#title 
+        newscat = request.POST.get('newscat')#yangilik kategorysi
+        newstxtshort = request.POST.get('newstxtshort')#qisqacha matn
+        newstxt = request.POST.get('newstxt')#to'liq matn
+        newsid = request.POST.get('newscat')#yangilik ID raqami, avtomatik holatda category ID asosda olib kelinadi
+        tag = request.POST.get('tag')#yangilik tagi
+        if newstitle == "" or newstxtshort == "" or newstxt == "" or newscat=="":
+            error = "All Fields Required"
+            return render(request,'back/error.html',{'error':error})
+        try:
+            myfile = request.FILES['myfile']#file yuklash qismi, rasm
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name,myfile)
+            url = fs.url(filename)#media faylni bazadagi url manzili, saqlansin
+            if str(myfile.content_type).startswith("image"):#rasm nomi yoki fayl formati rasm formatga teng bo'lsa, img, png, jpg
+                if myfile.size < 50000000:#fayl o'lchami 5 MB dan kichik bo'lsa
+                    newsname = SubCat.objects.get(pk=newsid).name#ketma ketlik
+                    b = News.objects.get(pk=pk)
+                    fss = FileSystemStorage()
+                    fss.delete(b.picname)
+                    b.name = newstitle 
+                    b.short_txt = newstxtshort
+                    b.body_txt = newstxt 
+                    b.picname = filename 
+                    b.picurl = url 
+                    b.catname = newsname 
+                    b.catid = newsid 
+                    b.tag = tag 
+                    b.act = 0 
+                    b.save()#to'liq yangilik saqlab qo'yilsin
+                    return redirect('news_list')#yangiliklar ro'yxatiga qaytarilsin
+                else:#file formati 5 mbdan katta bo'lsa
+                    fs = FileSystemStorage()
+                    fs.delete(filename)#faylni avtomatik o'chirsin , tanlashga ruxsat bermasin
+                    error = "Your File Is Bigger Than 5 MB"
+                    return render(request,'back/error.html',{'error':error})
+            else:#img, jpg, png format bo'lmasachi, oddiy fayl doc fayl, pdf fayl
+                fs = FileSystemStorage()
+                fs.delete(filename)#faylni o'chirsin
+                error = "Your File Not Supported"#mos tushmadi degan xatolik bersin
+                return render(request,'back/error.html',{'error':error})
+        except:
+            newsname = SubCat.objects.get(pk=newsid).name#query ketma ketlikka olib kelinsin
+            b = News.objects.get(pk=pk)
+            b.name = newstitle
+            b.short_txt = newstxtshort 
+            b.body_txt = newstxt 
+            b.catname = newsname 
+            b.catid = newsid 
+            b.tag = tag 
+            b.save()#hech qanday fayl bilan ishlamasak ham yangilikni saqlab qo'yish imkoni bo'lsin
+            return redirect('news_list')#
+    return render(request,'back/news_edit.html',{'pk':pk,'news':news,'cat':cat})
+
+def news_publish(request,pk):
+    if not request.user.is_authenticated:
+        return redirect('mylogin')
+    news = News.objects.get(pk=pk)
+    news.act = 1 #activlashsin 
+    news.save()
+    return redirect('news_list')
